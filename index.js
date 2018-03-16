4fb72132de6d50ba9e9269ba741532c3a8ed9d6b
Cami Rodriguez <camiepisode@outlook.com>
2018-03-16 01:45:33 -0500

    checking if is beat folder

--- index.js
+++ index.js
@@ -2,7 +2,34 @@
 
 const program = require('commander');
 const { prompt } = require('inquirer');
-
+const finder = require('fs-finder');
+const originalFolderStructure = {
+  'firstLevel': [
+    'config',
+    'public',
+    'src'
+  ],
+  'srcLevel': [
+    'controllers',
+    'manage',
+    'routes',
+    'views'
+  ],
+  'routesLevel': [
+    'api',
+    'frontend'
+  ]
+};
+const fileStructure = {
+  'controllers': ['mainController.js'],
+  'manage':[
+    'components.js',
+    'dependencies.js',
+    'server.js',
+    'settings.js',
+    'utilities.js'
+  ]
+}
 const newProject = [
   {
     type: 'input',
@@ -66,16 +93,19 @@ program
     prompt(typeOfProject).then(choise => {
       switch (choise.type.toLocaleLowerCase()) {
         case 'project':
-          prompt(newProject).then(answers =>
-            console.log(answers));
+          prompt(newProject).then(answers => {
+            createNewProject(answers);
+          });
           break;
         case 'view':
-          prompt(newView).then(answers =>
-            console.log(answers));
+          prompt(newView).then(answers =>{
+            createNewView(answers);
+          });
           break;
         case 'api controller':
-          prompt(newApiController).then(answers =>
-            console.log(answers));
+          prompt(newApiController).then(answers =>{
+            createNewAPIController(answers);
+          });
           break;
         default:
           break;
@@ -103,7 +133,109 @@ program
     console.log(flags.firebase)
   });
 
-
-
 program.parse(process.argv);
 
+function createNewProject(data) {
+  if(isBeatFolder() === false){
+
+  }
+}
+
+function createNewView(data) {
+  if(isBeatFolder() === false){
+    
+  }
+}
+
+function createNewAPIController(data) {
+  if(isBeatFolder() === false){
+    
+  }
+}
+
+function isBeatFolder() {
+  var preserveFolderStructure = folderStructure();
+  var preservefilesStructure = filesStructure();
+
+  if(preservefilesStructure === true && preserveFolderStructure === true){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+const folderStructure = function () {
+  let currentDir = process.cwd();
+  
+  let foldersFirstLevel = finder.in(currentDir).findDirectories();
+  let foldersSrcLevel = finder.in(currentDir + '\\src').findDirectories();
+  let foldersRoutesLevel = finder.in(currentDir + '\\src\\routes').findDirectories();
+
+  if (foldersFirstLevel && foldersSrcLevel && foldersRoutesLevel) {
+    let preserveFirstLevel = false;
+    let preserveSrcLevel = false;
+    let preserveRoutesLevel = false;
+    let match = 0;
+
+    foldersFirstLevel.map((folder) => {
+      originalFolderStructure.firstLevel.map((structureFolder) => {
+        if (folder.indexOf(structureFolder) > 0) match++;
+      })
+    })
+
+    if (originalFolderStructure.firstLevel.length > match) {
+      return false;
+    }
+
+    match = 0;
+    foldersSrcLevel.map((folder) => {
+      originalFolderStructure.srcLevel.map((structureFolder) => {
+        if (folder.indexOf(structureFolder)>0) match++;
+      })
+    })
+
+    if (originalFolderStructure.srcLevel.length > match) {
+      return false;
+    }
+
+    match = 0;
+    foldersRoutesLevel.map((folder) => {
+      originalFolderStructure.routesLevel.map((structureFolder) => {
+        if (folder.indexOf(structureFolder)) match++;
+      })
+    })
+
+    if (originalFolderStructure.routesLevel.length > match) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return false;
+};
+
+const filesStructure = function(){
+  let currentDir = process.cwd();
+  let manageFiles = finder.in(currentDir + '\\src\\manage').findFiles();
+
+  if(manageFiles){
+    let preserveManageFiles = false;
+    let preserveControllersFiles = false;
+    let match = 0;
+
+    manageFiles.map((manageFile) => {
+      fileStructure.manage.map((manageFileStructure) => {
+        if (manageFile.indexOf(manageFileStructure) > 0) match++;
+      })
+    })
+
+    if (fileStructure.manage.length > match) {
+      return false;
+    }
+
+    return true;
+  }
+  
+}
