2f4be5e8d6b7b6f323c21eaa40a8a1961343926a
Cami Rodriguez <camiepisode@outlook.com>
2018-03-15 19:10:38 -0500

    including primary options

--- index.js
+++ index.js
@@ -1,50 +1,109 @@
+#!/usr/bin/env node
+
 const program = require('commander');
 const { prompt } = require('inquirer');
 
-const questions = [
+const newProject = [
   {
-    type : 'input',
-    name : 'firstname',
-    message : 'Enter firstname ...'
+    type: 'input',
+    name: 'name',
+    message: 'Project name:'
   },
   {
-    type : 'input',
-    name : 'lastname',
-    message : 'Enter lastname ...'
+    type: 'confirm',
+    name: 'firebase',
+    message: 'Do you want to use firebase? Y/n: '
   },
   {
-    type : 'input',
-    name : 'phone',
-    message : 'Enter phone number ...'
+    type: 'confirm',
+    name: 'api',
+    message: 'Want to use Web API? Y/n: '
   },
   {
-    type : 'input',
-    name : 'email',
-    message : 'Enter email address ...'
+    type: 'confirm',
+    name: 'frontend',
+    message: 'Want to use a Frontend? Y/n:'
   }
 ];
+
+const newView = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'View name: '
+  },
+];
+
+const newApiController = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Controller name: '
+  },
+]
+
+const typeOfProject = [
+  {
+    type: 'list',
+    name: 'type',
+    message: 'What type of project',
+    choices: [
+      'Project',
+      'View',
+      'API Controller'
+    ]
+  }
+]
 
 program
   .version('0.0.1')
   .description('Contact management system');
 
 program
-  .command('addContact')
-  .alias('a')
-  .description('Add a contact')
-  .action((firstname, lastname, phone, email) => {
-    prompt(questions).then(answers =>{
-      console.log(answers.firstname)
+  .command('new')
+  .alias('n')
+  .action(() => {
+    prompt(typeOfProject).then(choise => {
+      switch (choise.type.toLocaleLowerCase()) {
+        case 'project':
+          prompt(newProject).then(answers =>
+            console.log(answers));
+          break;
+        case 'view':
+          prompt(newView).then(answers =>
+            console.log(answers));
+          break;
+        case 'api controller':
+          prompt(newApiController).then(answers =>
+            console.log(answers));
+          break;
+        default:
+          break;
+      }
+
+
     });
   });
 
 program
-  .command('getContact <name>')
-  .alias('r')
-  .description('Get contact')
-  .action(name => {
-    console.log('command');
+  .command('new <type>')
+  .description('New project')
+  .action(() => {
+    prompt(newProject).then(answers =>
+      console.log(answers));
   });
+
+program
+  .command('new <type> <name>')
+  .option('-f, --firebase')
+  .option('-a, --api')
+  .option('-w, -web')
+  .description('New project')
+  .action((type, name, flags) => {
+    console.log(flags.firebase)
+  });
+
+
 
 program.parse(process.argv);
 
